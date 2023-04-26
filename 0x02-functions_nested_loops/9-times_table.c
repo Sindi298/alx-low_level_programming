8fc80446e59a076c2c55363be6a1f5c228b44f53
Sindiswa <sindiswamsubo71@gmail.com>
2023-04-26 15:37:59 -0700

    101-natural.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,19 @@
  */
 void times_table(void)
 {
-	int number;
-
-	printf("Enter the number: ");
-	scanf("%d", &number);
-
-	int c;
-
-	for (c = 0; c <= 10; c++)
-	{
-		int product = number * c;
-
-		printf("%d*%d = %d\n", number, c, product);
-	}
+int number;
+printf("Enter the number: ");
+scanf("%d", &number);
+int c;
+int j;
+int product;
+for (c = 0; c <= 10; c++)
+{
+for (j = 0; j <= 10; j++)
+{
+product = number * j;
+printf("%d ", product);
+}
+printf("\n");
+}
 }
