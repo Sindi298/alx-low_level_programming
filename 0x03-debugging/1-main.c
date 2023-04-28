2f01815fd66435d1cc53f5394b3bc5be40d6be3b
Sindiswa <sindiswamsubo71@gmail.com>
2023-04-27 21:03:09 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,10 +6,8 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
 i = 0;
-
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
@@ -17,4 +15,3 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
-
